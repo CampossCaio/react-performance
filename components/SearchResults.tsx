408a0c7a755159e3ae6448dc5fb257097cf58609
caiocampos-hotmart <82822777+caiocampos-hotmart@users.noreply.github.com>
2021-09-20 08:12:59 -0300

    feat: create SearchResults component

---
+++ components/SearchResults.tsx
@@ -0,0 +1,58 @@
+import { useMemo } from 'react';
+import { List, ListRowRenderer } from 'react-virtualized';
+import { ProductItem } from "./ProductItem";
+
+type SearchResultsProps = {
+  results: Array<{
+    id: number;
+    price: number;
+    title: string;
+  }>,
+  onAddWishList: (id: number) => void;
+}
+
+export function SearchResults({ results, onAddWishList }: SearchResultsProps) {
+
+  const totalPrice = useMemo(() => {
+    return results.reduce((total, product) => {
+      return total + product.price;
+    },0)
+  },[results]);
+
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+     <div key={key} style={style}>
+        <ProductItem 
+          product={results[index]}
+          onAddWishList={onAddWishList}
+        />
+     </div>
+    )
+  }
+
+  return (
+    <div>
+      <h2>{totalPrice}</h2>
+
+       <List 
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5} // Quantidade de itens pré carregados, tanto no início quanto no final da lista
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+       />
+      {
+        // results.map(product => {
+        //   return (
+        //     <ProductItem 
+        //       key={product.id} 
+        //       product={product}
+        //       onAddWishList={onAddWishList}
+        //     />
+        //   );
+        // })
+      }
+    </div>
+  )
+}
